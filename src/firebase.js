bda75e96de806f24e59595f9fedb097e172d061e
Eunjin09 <77655749+Eunjin09@users.noreply.github.com>
2022-06-02 15:39:21 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBopPCSge0pf3PvWAMVJgkz_zmb_cz3eK8",
-  authDomain: "sparta-react-basic-a3d91.firebaseapp.com",
-  projectId: "sparta-react-basic-a3d91",
-  storageBucket: "sparta-react-basic-a3d91.appspot.com",
-  messagingSenderId: "320412729298",
-  appId: "1:320412729298:web:ee60c3244d496c7116120b",
-  measurementId: "G-QSW8HTWXKR"
+  
 };
 
 // firebaseConfig 정보로 firebase 시작
